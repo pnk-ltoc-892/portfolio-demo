d53c04c4f43655db82b50db4f99f1a9b27c3eaea
Pankaj Singh <165186164+pnk-ltoc-892@users.noreply.github.com>
2024-10-26 02:18:03 +0530

    Final Hero Section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,37 +1,64 @@
-import Link from 'next/link'
 import React from 'react'
 import { Spotlight } from './ui/Spotlight.tsx'
-import { Button } from './ui/moving-border.tsx'
+import { TypewriterEffectSmooth } from './ui/typewriter-effect.tsx';
+import { TextGenerateEffect } from './ui/text-generate-effect.tsx';
+
+const words = [
+    {
+        text: "I",
+    },
+    {
+        text: "Am",
+    },
+    {
+        text: "Pankaj",
+        classname: "text-neutral-400"
+    },
+    {
+        text: "Singh",
+        classname: "text-neutral-400"
+    }
+]
+
+const intro = `a passionate full-stack developer, I love crafting & building innovative business solutions and addressing real-world problems through technology. With a knack for problem-solving, building stuff and a love for coding, I thrive on transforming ideas into functional realities. Dive in to know more about me, explore my projects, skills and things i am working on...
+`;
 
 const HeroSection = () => {
     return (
-        <div
-            className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'
-        >
-            <div className="p-4 relative z-10 w-full text-center" >
+        <div className="h-[50rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
             <Spotlight
-                className="-top-40 left-0 md:left-80 md:-top-20"
+                className="-top-40 left-0 md:left-60 md:-top-40"
                 fill="white"
             />
-                <h1
-                className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-100 to-neutral-500"
-                >Master the art of music</h1>
-                <p
-                className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto"
-                >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
-                <div className="mt-4">
-                    <Link href={"/courses"}>
-                    <Button
-                            borderRadius="1.75rem"
-                            className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
-                        >
-                            Explore Courses
-                    </Button>
-                    </Link>
+            <Spotlight
+                className="top-0 left-0 md:left-60 md:top-10"
+                fill="gray"
+            />
+            <Spotlight
+                className="-top-40 -left-20 md:-left-32 md:-top-20 h-screen"
+                fill="gray"
+            />
+
+            <div className="p-4 max-w-[70rem] flex flex-col justify-center items-center mx-auto relative z-10 w-full pt-20 md:pt-0">
+
+                <span className='text-5xl font-bold'>Hi</span>
+                <div className='mx-8'>
+                    <TypewriterEffectSmooth words={words} />
                 </div>
+                <div>
+                    <TextGenerateEffect words={intro} />
+                </div>
+
+                {/* <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+                    Spotlight effect is a great way to draw attention to a specific part
+                    of the page. Here, we are drawing the attention towards the text
+                    section of the page. I don&apos;t know why but I&apos;m running out of
+                    copy.
+                </p> */}
             </div>
         </div>
-    )
+    );
 }
+
 
 export default HeroSection
